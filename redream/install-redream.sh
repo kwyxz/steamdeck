d07fd185a9ad231cf37d4d819f9618b0e7f0456a
kwyxz <kwyxz@kwyxz.org>
2022-09-12 21:16:46 -0700

    Cosmetic

--- redream/install-redream.sh
+++ redream/install-redream.sh
@@ -8,5 +8,5 @@ printf "%s\n" " * downloading Redream"
 rm -f ${REDREAM_TMP}
 REDREAM_DEV="$(curl -s ${REDREAM_URL} | grep x86_64-linux | grep -- "v.\..\..-.*-.*\.tar\.gz" | head -1 | cut -d '"' -f2 | cut -d '/' -f3)"
 curl -s ${REDREAM_URL}/${REDREAM_DEV} -o ${REDREAM_TMP}
-printf "%s\n" "* installing Redream"
+printf "%s\n" " * installing Redream"
 tar zxf ${REDREAM_TMP} -C ${REDREAMPATH}/
