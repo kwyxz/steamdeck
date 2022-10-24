ff17f3cc33376a9e635d9f21eb6e10bb35f8e71f
kwyxz <kwyxz@kwyxz.org>
2022-10-24 12:00:11 -0700

    Make it even quieter

--- duckstation/install-duckstation.sh
+++ duckstation/install-duckstation.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-wget -q https://github.com/stenzek/duckstation/releases/download/preview/DuckStation-x64.AppImage -O /home/deck/duckstation/DuckStation-x64.AppImage
+wget https://github.com/stenzek/duckstation/releases/download/preview/DuckStation-x64.AppImage -O /home/deck/duckstation/DuckStation-x64.AppImage 2>/dev/null
 chmod 755 /home/deck/duckstation/DuckStation-x64.AppImage
