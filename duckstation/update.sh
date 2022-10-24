c8ac788f048ee189cdc61158935a7ff204420ad3
kwyxz <kwyxz@kwyxz.org>
2022-10-24 11:54:13 -0700

    Added script to install Duckstation

---
+++ duckstation/update.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+wget -q https://github.com/stenzek/duckstation/releases/download/preview/DuckStation-x64.AppImage -O /home/deck/duckstation/DuckStation-x64.AppImage
+chmod 755 /home/deck/duckstation/DuckStation-x64.AppImage
