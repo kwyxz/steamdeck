d62385ea2b01341a48beca32a7c37ded2db648e9
kwyxz <kwyxz@kwyxz.org>
2022-11-13 23:33:10 -0800

    Minor change to unify scripts

--- duckstation/install-duckstation.sh
+++ duckstation/install-duckstation.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
-wget https://github.com/stenzek/duckstation/releases/download/preview/DuckStation-x64.AppImage -O /home/deck/duckstation/DuckStation-x64.AppImage 2>/dev/null
-chmod 755 /home/deck/duckstation/DuckStation-x64.AppImage
+EMUPATH="/home/deck/duckstation"
+REPO="https://github.com/stenzek/duckstation"
+LATEST="preview"
+
+echo -n "Downloading Duckstation AppImage ${LATEST} ... " && wget ${REPO}/releases/download/${LATEST}/DuckStation-x64.AppImage -O ${EMUPATH}/DuckStation-x64.AppImage 2>/dev/null && chmod 755 ${EMUPATH}/DuckStation-x64.AppImage && echo "OK" || echo "ERROR"
