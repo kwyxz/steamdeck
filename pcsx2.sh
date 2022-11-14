894156bef27cb11edca2a5569d57de8074b33f1d
kwyxz <kwyxz@kwyxz.org>
2022-11-13 22:43:38 -0800

    Use pcsx2 AppImage

--- pcsx2.sh
+++ pcsx2.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-/usr/bin/flatpak run net.pcsx2.PCSX2 --fullscreen --nogui "${1}"
+#/usr/bin/flatpak run net.pcsx2.PCSX2 --fullscreen --nogui "${1}"
+/home/deck/pcsx2/pcsx2-x64.AppImage -nogui -bigpicture -fullscreen -- "${1}"
