dcd3add368c9bcea16c6fcd7a75a2dee904dd40d
kwyxz <kwyxz@kwyxz.org>
2023-01-21 16:54:40 -0800

    Not sure we need an argument here but it cannot hurt

--- retroarch.sh
+++ retroarch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-/usr/bin/flatpak run org.libretro.RetroArch -c /home/deck/.config/retroarch/retroarch.cfg
+/usr/bin/flatpak run org.libretro.RetroArch -c /home/deck/.config/retroarch/retroarch.cfg ${1}
