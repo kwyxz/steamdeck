1a3f381fd6518ac24bc12432d4cb5d42bc5ef7b6
Benjamin Francois <Benjamin.Francois@disneyanimation.com>
2023-08-03 16:24:50 -0700

    updated rompath for MAME

--- mame.sh
+++ mame.sh
@@ -8,4 +8,4 @@ FLATPAK="/usr/bin/flatpak"
 MAME="org.mamedev.MAME"
 
 # Call the Flatpak launcher and give the arguments
-"${FLATPAK}" run "${MAME}" -verbose -rompath /home/deck/Emulation/roms/arcade/ -inipath /home/deck/.mame/ -video bgfx -bgfx_path /home/deck/.mame/bgfx -bgfx_backend vulkan -bgfx_screen_chains crt-geom-deluxe "${@}"
+"${FLATPAK}" run "${MAME}" -verbose -rompath /home/deck/Emulation/roms/arcade/;/home/deck/Emulation/roms/model1/;/home/deck/Emulation/roms/model2/ -inipath /home/deck/.mame/ -video bgfx -bgfx_path /home/deck/.mame/bgfx -bgfx_backend vulkan -bgfx_screen_chains crt-geom-deluxe "${@}"
