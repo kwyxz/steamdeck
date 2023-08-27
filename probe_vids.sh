858639f7a627ccdc4384b99bd790d25999479761
kwyxz <kwyxz@kwyxz.org>
2023-08-27 11:30:48 -0700

    added script to probe video previews

---
+++ probe_vids.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+LOGFILE=${HOME}/${0}.log
+MEDIAPATH=/home/deck/Emulation/tools/downloaded_media
+
+echo -e "=== list of problematic video files ===\n" > ${LOGFILE}
+
+for FICH in ${MEDIAPATH}/*/videos/*.mp4
+do
+  if ! ffprobe ${FICH} >/dev/null 2>/dev/null
+  then
+    echo ${FICH} &>> ${LOGFILE}
+  fi
+done
