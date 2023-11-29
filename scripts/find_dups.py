92fc8cc16c1461e7b6bb45cbb09b5460e7952e84
kwyxz <kwyxz@kwyxz.org>
2023-11-28 22:17:19 -0800

    recommend the best potential ROM

--- scripts/find_dups.py
+++ scripts/find_dups.py
@@ -7,31 +7,52 @@ find duplicates in DAT files
 import argparse
 import xml.etree.ElementTree as ET
 
-parser = argparse.ArgumentParser(
-    prog='find_dup',
-    description='find duplicates in emulationstation XML files')
-parser.add_argument('xmlfile', type=argparse.FileType('r'))
-args = parser.parse_args()
+COUNTRY_LIST=['(USA)','(World)','(Europe)','(France)','(Japan) (En)','(Japan) [T-En']
 
-tree = ET.parse(args.xmlfile)
-root = tree.getroot()
-
-game_list = {}
-dups = {}
-
-for game in root.findall('game'):
-    name = game.find('name').text
-    rom = game.find('path').text
-    if (' (Disc ' or ' (Disk ') not in rom:
-        if name in game_list:
-            if name in dups:
-                dups[name].append(rom)
+def list_dups(root):
+    dups = {}
+    game_list = {}
+    for game in root.findall('game'):
+        name = game.find('name').text
+        rom = game.find('path').text
+        if (' (Disc ' or ' (Disk ') not in rom:
+            if name in game_list:
+                if name in dups:
+                    dups[name].append(rom)
+                else:
+                    dups[name] = [game_list[name],rom]
             else:
-                dups[name] = [game_list[name],rom]
-        else:
-            game_list[name] = rom
+                game_list[name] = rom
+    return dups
 
-for gamename, romname in sorted(dups.items()):
-    print(f"=== GAME : {gamename}")
-    for romfile in romname:
-        print(f" \"{romfile}\"")
+def find_best(roms,countries):
+    for country in countries:
+        for rom in roms:
+            if country in rom:
+                return rom
+    return False
+
+def print_dups(dups):
+    for gamename, romnames in dups:
+        print(f"=== GAME : {gamename}")
+        best = find_best(romnames,COUNTRY_LIST)
+        for rom in romnames:
+            if best and (rom == best):
+                print(f"\033[92m{best.split('/')[1]}\033[0m")
+            else:
+                print(rom.split('/')[1])
+
+def main():
+    duplicates = sorted(list_dups(ROOT).items())
+    print_dups(duplicates)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog='find_dup',
+        description='find duplicates in emulationstation XML files')
+    parser.add_argument('xmlfile', type=argparse.FileType('r'))
+
+    ARGS = parser.parse_args()
+    TREE = ET.parse(ARGS.xmlfile)
+    ROOT = TREE.getroot()
+    main()
