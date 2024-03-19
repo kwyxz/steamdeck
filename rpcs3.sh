2e4fa09833a9b09fcf75c2a3d72d69d5da48e18d
kwyxz <kwyxz@kwyxz.org>
2024-03-19 00:17:58 -0700

    fixed xemu script + rpcs3 script

--- rpcs3.sh
+++ rpcs3.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 
-/var/lib/flatpak/exports/bin/net.rpcs3.RPCS3 --no-gui "${1}"
+if test -f Applications/rpcs3.AppImage; then
+  /home/deck/Applications/rpcs3.AppImage --no-gui "${1}"
+else
+  /var/lib/flatpak/exports/bin/net.rpcs3.RPCS3 --no-gui "${1}"
+fi
--- xemu.sh
+++ xemu.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
-### flatpak option
-#__GLX_VENDOR_LIBRARY_NAME=mesa MESA_LOADER_DRIVER_OVERRIDE=zink GALLIUM_DRIVER=zink /usr/bin/flatpak run app.xemu.xemu -full-screen -dvd_path "${1}"
-#/usr/bin/flatpak run app.xemu.xemu -full-screen -dvd_path "${1}"
-### AppImage option
+if test -f /home/deck/Applications/xemu.AppImage; then
 #__GLX_VENDOR_LIBRARY_NAME=mesa MESA_LOADER_DRIVER_OVERRIDE=zink GALLIUM_DRIVER=zink /home/deck/xemu/xemu.AppImage -full-screen -dvd_path "${1}"
-#/home/deck/xemu/xemu.AppImage -full-screen -dvd_path "${1}"
+  /home/deck/Applications/xemu.AppImage -full-screen -dvd_path "${1}"
+else
+#__GLX_VENDOR_LIBRARY_NAME=mesa MESA_LOADER_DRIVER_OVERRIDE=zink GALLIUM_DRIVER=zink /usr/bin/flatpak run app.xemu.xemu -full-screen -dvd_path "${1}"
+  /usr/bin/flatpak run app.xemu.xemu -full-screen -dvd_path "${1}"
+fi
