80dbe3c099fddef8739b04ab4a74542e7473d65e
kwyxz <kwyxz@kwyxz.org>
2024-03-18 23:25:33 -0700

    removed supermodel build, use the flatpak

--- update-all.sh
+++ update-all.sh
@@ -4,9 +4,8 @@
 ./pcsx2/install-pcsx2.sh
 ./cemu/install-cemu.sh
 ./citra/install-citra.sh
-./supermodel/install-supermodel.sh
 ./redream/install-redream.sh
-./xemu/install-xemu.sh
+#./xemu/install-xemu.sh
 ./xenia/install-xenia.sh
 ./yuzu/install-yuzu.sh
 git pull && git submodule update --recursive --remote
