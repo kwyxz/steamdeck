af1a8ed21e8318f299418c1b44c59d90f6da2b30
kwyxz <kwyxz@kwyxz.org>
2024-05-05 16:45:21 -0700

    make sure we only look through folders

--- scripts/media_helper.py
+++ scripts/media_helper.py
@@ -31,11 +31,11 @@ def find_missing(root,mediapath):
             rom = rom.stem
         games[rom] = {}
         for file in os.scandir(mediapath):
-            fold = file.name
-            if ((fold=='metadata.txt') or (fold=='systeminfo.txt')):
-                break
-            else:
+            if os.path.isdir(file):
+                fold = file.name
                 games[rom][fold] = False
+            else:
+                break
             if fold == 'videos':
                 media_format = ['mp4','flv','mkv']
             else:
