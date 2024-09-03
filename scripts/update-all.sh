b479bf84ba057fb7deea9e72f0032d94420993fb
kwyxz <kwyxz@kwyxz.org>
2024-09-02 23:52:50 -0700

    make redream script more unified with others

--- scripts/update-all.sh
+++ scripts/update-all.sh
@@ -7,7 +7,7 @@ git checkout main
 /home/deck/Applications/citra/install-citra.sh
 /home/deck/Applications/redream/install-redream.sh
 /home/deck/Applications/ryujinx/install-ryujinx.sh
-/home/deck/Applications/suyu/install-suyu.sh
+#/home/deck/Applications/suyu/install-suyu.sh
 /home/deck/Applications/xemu/install-xemu.sh
 /home/deck/Applications/xenia/install-xenia.sh
 git pull && git submodule update --recursive --remote
